c0998f415ce68e3efdb22f9851d357f70870f132
tionbail <m.a.panarin122@gmail.com>
2026-03-24 21:09:12 +0400

    first commit

---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SearchTests {
+    @Test
+
+    @Disabled
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+    }
+
+    @Test
+    void successfulSearchTestYandex() {
+        open("https://ya.ru/");
+        $("[name=text]").setValue("selenide").pressEnter();
+        $("[class=content]").shouldHave(text("https://selenide.org"));
+    }
+}
