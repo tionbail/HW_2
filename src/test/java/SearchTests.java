45e969421fb561918f956deec4d197efe31cec87
tionbail <m.a.panarin122@gmail.com>
2026-03-25 11:00:56 +0400

    second commit

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -16,7 +16,7 @@ public class SearchTests {
 
     @Test
     void successfulSearchTestYandex() {
-        open("https://yahoo.ru/");
+        open("https://yahooo.ru/");
         $("[name=text]").setValue("selenide").pressEnter();
         $("[class=content]").shouldHave(text("https://selenide.org"));
     }
